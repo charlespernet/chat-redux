1ef5f6ea74e691676743249332615c6af5a9be8d
Charles Pernet <charlespernet@gmail.com>
2019-03-14 17:23:03 +0100

    Add redux logger

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, combineReducers } from 'redux';
+import { applyMiddleware, createStore, combineReducers } from 'redux';
+import logger from 'redux-logger';
 
 // internal modules
 import App from './components/app';
@@ -28,7 +29,7 @@ const reducers = combineReducers({
 
 // render an instance of the component in the DOM
 ReactDOM.render(
-  <Provider store={createStore(reducers)}>
+  <Provider store={createStore(reducers, applyMiddleware(logger))}>
     <App />
   </Provider>,
   document.getElementById('root')
