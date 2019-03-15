38c567080b5411e35e0b42da1673e52030bc8fcb
Charles Pernet <charlespernet@gmail.com>
2019-03-15 10:57:38 +0100

    Add little css

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -4,7 +4,10 @@ import MessageList from '../containers/message_list';
 const App = () => {
   return (
     <div className="app">
-      <div className="channel-list"></div>
+      <div className="channel-list">
+        <div className="channel-list-border"></div>
+        <div className="channel-list-body"></div>
+      </div>
       <MessageList />
     </div>
   );
